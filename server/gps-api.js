50db83e9a1f0679f6e4a420320329831a3e238f3
John Grese <grese@oath.com>
2018-07-30 01:24:55 -0700

    build image & push to ECR

--- server/gps-api.js
+++ server/gps-api.js
@@ -2,8 +2,8 @@ const express = require('express');
 const gpsApi = express();
 const server = require('http').Server(gpsApi);
 const io = require('socket.io')(server);
-const redis = require('redis');
-const rclient = redis.createClient();
+//const redis = require('redis');
+//const rclient = redis.createClient();
 const {logger} = require('./logger');
 
 // Just a dummy route.  This can be removed once we start adding real routes.
@@ -17,12 +17,12 @@ io.on('connection', function (socket) {
   logger.info(socket.id);
   socket.on('receive-gps', function (data) {
 
-    rclient.hmset( socket.id, 'latitude',data.lat,'longitude',data.lon, function(err) {
-      if(err)
-        logger.info('redis client error', err);
-      else
-        logger.info(data);
-    });
+    // rclient.hmset( socket.id, 'latitude',data.lat,'longitude',data.lon, function(err) {
+    //   if(err)
+    //     logger.info('redis client error', err);
+    //   else
+    //     logger.info(data);
+    // });
   });
 });
 
